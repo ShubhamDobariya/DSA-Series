4eb380721ff478e6d81a56a76f627a7a91e8cfce
Shubham Dobariya <shubhamdobariyas880@gmail.com>
2025-01-13 18:24:44 +0530

    Add Recursion Folder

---
+++ Recursion/FibonacciNumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int FibonacciNum(int n) // TC = O(2^n) , SC = O(n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return FibonacciNum(n - 1) + FibonacciNum(n - 2);
+}
+
+int main()
+{
+    int n = 4;
+    cout << FibonacciNum(n) << endl;
+    return 0;
+}
